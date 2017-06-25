e553363fcfcd4908797832f208361a02f0c2dbde
cabinsky <258344941@qq.com>
2017-06-25 23:34:46 +0800

    my test

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             { test: /\.css$/, loader: 'style-loader!css-loader' },
            // { test: /\.js$/, loader: 'jsx-loader?harmony' },
             { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+            //{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
         ]
     },
     //其它解决方案配置
