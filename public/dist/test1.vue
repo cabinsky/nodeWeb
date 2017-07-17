f6e43c078d1da4688148cbed0d28ad18632c31e8
cabinsky <258344941@qq.com>
2017-07-17 23:45:11 +0800

    my test

---
+++ public/dist/test1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      {{text}}
+  </div>
+
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+      var test1 = Vue.extend({
+          template: template, // the variable template will be injected
+          data: function() {
+              return {"text": "Ok"};
+          }
+      });
+      return {
+        get:function(){
+            return test1;
+            }
+        };
+      }
+    );
+</script>
---
+++ public/dist/test2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      {{text}}
+  </div>
+
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+      Vue.component("test2-component", {
+          template: template, // the variable template will be injected
+          data: function() {
+              return {"text": "test2"};
+          }
+      });
+    });
+</script>
